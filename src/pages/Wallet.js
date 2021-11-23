de2c55b6215325761c7c0708e8678dda72fb3e83
DanMondaini <danielmondaini@outlook.com>
2021-11-23 17:32:13 -0300

    Corrige requisito 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -24,26 +24,13 @@ class Wallet extends React.Component {
         <header>
           <h3 data-testid="email-field">{email}</h3>
           <section>
-            <p data-testid="total-field">{totalWalletValue}</p>
+            <p data-testid="total-field">
+              {totalWalletValue || 0}
+            </p>
             <p data-testid="header-currency-field">BRL</p>
           </section>
         </header>
         <WalletForm />
-        <table>
-          <tbody>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-          </tbody>
-        </table>
       </div>
     );
   }
